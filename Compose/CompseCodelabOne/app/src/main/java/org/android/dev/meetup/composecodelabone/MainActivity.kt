342703fad9bcb7be3efcf601a291ae1fb3c9b1ca
Marshall Gates <mggates@gmail.com>
2021-10-28 22:18:47 -0400

    Updated fro steps 9 10 11 and 12
    Createing a performant lasy list
    Persisting state
    Animating your list
    Styling and Themeing your app

--- Compose/CompseCodelabOne/app/src/main/java/org/android/dev/meetup/composecodelabone/MainActivity.kt
+++ Compose/CompseCodelabOne/app/src/main/java/org/android/dev/meetup/composecodelabone/MainActivity.kt
@@ -1,34 +1,44 @@
 package org.android.dev.meetup.composecodelabone
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.foundation.layout.Column
-import org.android.dev.meetup.composecodelabone.ui.theme.ComposeCodelabOneTheme
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.material.*
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.graphics.Color
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import org.android.dev.meetup.composecodelabone.ui.theme.ComposeCodelabOneTheme
+import androidx.compose.material.*
 import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.text.font.FontWeight
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyApp {
-                MyScreenContent()
+                Greetings()
             }
         }
     }
@@ -37,8 +47,35 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MyApp(content: @Composable () -> Unit) {
     ComposeCodelabOneTheme {
-        Surface(color = Color.Yellow) {
-            content()
+        Surface {
+            var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
+
+            if (shouldShowOnboarding) {
+                OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+            } else {
+                content()
+            }
+
+        }
+    }
+}
+
+@Composable
+fun OnboardingScreen(onContinueClicked: () -> Unit) {
+
+    Surface {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Welcome to the Basics Codelab!")
+            Button(
+                modifier = Modifier.padding(vertical = 24.dp),
+                onClick = onContinueClicked
+            ) {
+                Text("Continue")
+            }
         }
     }
 }
@@ -52,6 +89,16 @@ fun NameList(names: List<String>, modifier: Modifier = Modifier) {
         }
     }
 }
+
+@Composable
+private fun Greetings(names: List<String> = List(1000) { "Android #$it" } ) {
+    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
+        items(items = names) { name ->
+            Greeting(name = name)
+        }
+    }
+}
+
 @Composable
 fun MyScreenContent(names: List<String> = List(1000) { "Android #$it" }) {
     val counterState = remember { mutableStateOf(0) }
@@ -68,7 +115,7 @@ fun MyScreenContent(names: List<String> = List(1000) { "Android #$it" }) {
 }
 
 @Composable
-fun Greeting(name: String) {
+fun Greeting_old(name: String) {
     var isSelected by remember { mutableStateOf(false) }
     val backgroundColor by animateColorAsState(if (isSelected) Color.Red else Color.Transparent)
     val textColor by animateColorAsState(if (isSelected) Color.White else Color.Black)
@@ -78,12 +125,46 @@ fun Greeting(name: String) {
         color = textColor,
         style = MaterialTheme.typography.body1,
         modifier = Modifier
-            .padding(24.dp)
+            .padding(vertical = 4.dp, horizontal = 8.dp)
             .background(color = backgroundColor)
             .clickable(onClick = { isSelected = !isSelected })
     )
 }
+@Composable
+private fun Greeting(name: String) {
+    var expanded by remember { mutableStateOf(false) }
 
+    val extraPadding by animateDpAsState(
+        if (expanded) 48.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
+
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(modifier = Modifier
+                .weight(1f)
+                .padding(bottom = extraPadding.coerceAtLeast(0.dp))
+            ) {
+                Text(text = "Hello, ")
+                Text(text = name, style = MaterialTheme.typography.h4.copy(
+                        fontWeight = FontWeight.ExtraBold
+                    )
+                )
+            }
+            OutlinedButton(
+                onClick = { expanded = !expanded }
+            ) {
+                Text(if (expanded) "Show less" else "Show more")
+            }
+        }
+    }
+}
 @Composable
 fun Counter(count: Int, updateCount: (Int) -> Unit) {
     Button(onClick = { updateCount(count+1) },
@@ -95,10 +176,18 @@ fun Counter(count: Int, updateCount: (Int) -> Unit) {
     }
 }
 
-@Preview("MyScreen preview")
+
+
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "DefaultPreviewDark"
+)
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
-    MyApp {
-        MyScreenContent()
+    ComposeCodelabOneTheme {
+        Greetings()
     }
 }
--- Compose/CompseCodelabOne/app/src/main/java/org/android/dev/meetup/composecodelabone/ui/theme/Theme.kt
+++ Compose/CompseCodelabOne/app/src/main/java/org/android/dev/meetup/composecodelabone/ui/theme/Theme.kt
@@ -5,14 +5,20 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
-private val DarkColorPalette = darkColors(
+private val DarkColorPalette_old = darkColors(
     primary = Purple200,
     primaryVariant = Purple700,
     secondary = Teal200
 )
-
-private val LightColorPalette = lightColors(
+private val DarkColorPalette = darkColors(
+    surface = Blue,
+    onSurface = Navy,
+    primary = Navy,
+    onPrimary = Chartreuse
+)
+private val LightColorPalette_old = lightColors(
     primary = Purple500,
     primaryVariant = Purple700,
     secondary = Teal200
@@ -25,6 +31,12 @@ private val LightColorPalette = lightColors(
     onBackground = Color.Black,
     onSurface = Color.Black,
     */
+)
+private val LightColorPalette = lightColors(
+    surface = Blue,
+    onSurface = Color.White,
+    primary = LightBlue,
+    onPrimary = Navy
 )
 
 @Composable
