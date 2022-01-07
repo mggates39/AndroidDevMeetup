c3a835e000cd600abf6cfa925c50d4a26732ad36
Marshall Gates <mggates@gmail.com>
2022-01-06 20:16:23 -0500

    The actual work for step 5
    I did not properly select qll the files for the last commitl.

--- Compose/ComposeLayout/app/src/main/java/org/android/dev/meetup/composelayout/MainActivity.kt
+++ Compose/ComposeLayout/app/src/main/java/org/android/dev/meetup/composelayout/MainActivity.kt
@@ -3,12 +3,26 @@ package org.android.dev.meetup.composelayout
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import org.android.dev.meetup.composelayout.ui.theme.ComposeLayoutTheme
 
 class MainActivity : ComponentActivity() {
@@ -16,27 +30,88 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeLayoutTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    PhotographerCard()
-                }
+                LayoutsCodelab()
+//                // A surface container using the 'background' color from the theme
+//                Surface(color = MaterialTheme.colors.background) {
+//                    PhotographerCard()
+//                }
             }
         }
     }
 }
 
 @Composable
-fun PhotographerCard() {
-    Column {
-        Text("Alfred Sisley", fontWeight = FontWeight.Bold)
-        // LocalContentAlpha is defining opacity level of its children
-        CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
-            Text("3 minutes ago", style = MaterialTheme.typography.body2)
+fun PhotographerCard(modifier: Modifier = Modifier) {
+    Row(modifier
+        .padding(8.dp)
+        .clip(RoundedCornerShape(4.dp))
+        .background(MaterialTheme.colors.surface)
+        .clickable(onClick = { /* Ignoring onClick */ })
+        .padding(16.dp)
+    ) {
+        Surface(
+            modifier = Modifier.size(50.dp),
+            shape = CircleShape,
+            color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
+        ) {
+            // Image goes here
+        }
+        Column(
+            modifier = Modifier
+                .padding(start = 8.dp)
+                .align(Alignment.CenterVertically)
+        ) {
+            Text("Alfred Sisley", fontWeight = FontWeight.Bold)
+            // LocalContentAlpha is defining opacity level of its children
+            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                Text("3 minutes ago", style = MaterialTheme.typography.body2)
+            }
         }
     }
 }
 
+@Composable
+fun LayoutsCodelab() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "LayoutsCodelab")
+                },
+                actions = {
+                    IconButton(onClick = { /* doSomething() */ }) {
+                        Icon(Icons.Filled.Favorite, contentDescription = null)
+                    };
+                    IconButton(onClick = { /* doSomething() */ }) {
+                        Icon(Icons.Filled.Home, contentDescription = null)
+                    }
+
+                }
+            )
+        }
+    ) { innerPadding ->
+        BodyContent(Modifier.padding(innerPadding).padding(8.dp))
+    }
+}
+
+@Composable
+fun BodyContent(modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        Text(text = "Hi there!")
+        Text(text = "Thanks for going through the Layouts codelab")
+    }
+}
+
 @Preview(showBackground = true)
+@Composable
+fun LayoutsCodelabPreview() {
+    ComposeLayoutTheme {
+        LayoutsCodelab()
+    }
+}
+
+
+@Preview(showBackground = false)
 @Composable
 fun PhotographerCardPreview() {
     ComposeLayoutTheme {
