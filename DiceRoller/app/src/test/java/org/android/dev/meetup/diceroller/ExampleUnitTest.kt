abab1531b7cb0793a5c9ad3d1c34fa6efe416d6c
Marshall Gates <mggates@gmail.com>
2022-01-20 19:56:46 -0500

    Added new unit test to Dice Roller to test roll function

--- DiceRoller/app/src/test/java/org/android/dev/meetup/diceroller/ExampleUnitTest.kt
+++ DiceRoller/app/src/test/java/org/android/dev/meetup/diceroller/ExampleUnitTest.kt
@@ -1,8 +1,8 @@
 package org.android.dev.meetup.diceroller
 
+import org.junit.Assert.assertTrue
 import org.junit.Test
 
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -11,7 +11,9 @@ import org.junit.Assert.*
  */
 class ExampleUnitTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun generates_number() {
+        val dice = Dice(6)
+        val rollResult = dice.roll()
+        assertTrue("The value of rollResult was not between 1 and 6", rollResult in 1..6)
     }
 }
