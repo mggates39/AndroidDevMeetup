57f23a682adaea90745510b6a0d37c2d737cf5c4
Marshall Gates <mggates@gmail.com>
2022-01-20 20:39:52 -0500

    Add id to layout for debug testing

--- Compose/Debugging/app/src/main/java/org/android/dev/meetup/debugging/MainActivity.kt
+++ Compose/Debugging/app/src/main/java/org/android/dev/meetup/debugging/MainActivity.kt
@@ -3,12 +3,15 @@ package org.android.dev.meetup.debugging
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 
 private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val helloTextView: TextView = findViewById(R.id.hello_world)
+        helloTextView.text = "Hello, debugging!"
         setContentView(R.layout.activity_main)
         logging()
         division()
