e74081f23db296bd7fb5d74f9628fb28868c6842
Marshall Gates <mggates@gmail.com>
2022-01-20 20:29:42 -0500

    Create a bug

--- Compose/Debugging/app/src/main/java/org/android/dev/meetup/debugging/MainActivity.kt
+++ Compose/Debugging/app/src/main/java/org/android/dev/meetup/debugging/MainActivity.kt
@@ -11,9 +11,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         logging()
+        division()
+    }
+    fun division() {
+        val numerator = 60
+        var denominator = 4
+        repeat(5) {
+            Log.v(TAG, "${numerator / denominator}")
+            denominator--
+        }
     }
 
     fun logging() {
-        Log.v(TAG, "Hello, world!")
+        Log.wtf(TAG, "WTF: It is really bad")
+        Log.e(TAG, "ERROR: a serious error like an app crash")
+        Log.w(TAG, "WARN: warns about the potential for serious errors")
+        Log.i(TAG, "INFO: reporting technical information, such as an operation succeeding")
+        Log.d(TAG, "DEBUG: reporting technical information useful for debugging")
+        Log.v(TAG, "VERBOSE: more verbose than DEBUG logs")
     }
 }
