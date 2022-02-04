8c38004f06c1c2d29ba6264f0b50c5548bc88e59
Marshall Gates <mggates@gmail.com>
2022-02-03 20:36:52 -0500

    Working step 7
    Customized layouts.  Building a Modifier function.

--- Compose/ComposeLayout/app/src/main/java/org/android/dev/meetup/composelayout/MainActivity.kt
+++ Compose/ComposeLayout/app/src/main/java/org/android/dev/meetup/composelayout/MainActivity.kt
@@ -19,8 +19,12 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.layout.FirstBaseline
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
 import kotlinx.coroutines.launch
@@ -109,6 +113,26 @@ fun BodyContent(modifier: Modifier = Modifier) {
 //    }
 }
 
+fun Modifier.firstBaselineToTop(
+    firstBaselineToTop: Dp
+) = this.then(
+    layout { measurable, constraints ->
+        val placeable = measurable.measure(constraints)
+
+        // Check the composable has a first baseline
+        check(placeable[FirstBaseline] != AlignmentLine.Unspecified)
+        val firstBaseline = placeable[FirstBaseline]
+
+        // Height of the composable with padding - first baseline
+        val placeableY = firstBaselineToTop.roundToPx() - firstBaseline
+        val height = placeable.height + placeableY
+        layout(placeable.width, height) {
+            // Where the composable gets placed
+            placeable.placeRelative(0, placeableY)
+        }
+    }
+)
+
 @Composable
 fun SimpleList() {
     // We save the scrolling position with this state that can also
@@ -175,19 +199,35 @@ fun ImageListItem(index: Int) {
     }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun LayoutsCodelabPreview() {
-    ComposeLayoutTheme {
-        LayoutsCodelab()
-    }
-}
-
+//@Preview(showBackground = true)
+//@Composable
+//fun LayoutsCodelabPreview() {
+//    ComposeLayoutTheme {
+//        LayoutsCodelab()
+//    }
+//}
+//
+//
+//@Preview(showBackground = false)
+//@Composable
+//fun PhotographerCardPreview() {
+//    ComposeLayoutTheme {
+//        PhotographerCard()
+//    }
+//}
 
 @Preview(showBackground = false)
 @Composable
-fun PhotographerCardPreview() {
+fun TextWithPaddingToBaselinePreview() {
     ComposeLayoutTheme {
-        PhotographerCard()
+        Text("Hi there!", Modifier.firstBaselineToTop(32.dp))
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TextWithNormalPaddingPreview() {
+    ComposeLayoutTheme {
+        Text("Hi there!", Modifier.padding(top = 32.dp))
     }
 }
