42ba6151d3354743ac419cb887831ff959a18f3b
Marshall Gates <mggates@gmail.com>
2022-02-17 18:22:46 -0500

    These are just some test projects

---
+++ Compose/MyApplication3/app/src/main/java/org/android/dev/meetup/myapplication3/MainActivity.kt
@@ -0,0 +1,11 @@
+package org.android.dev.meetup.myapplication3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
