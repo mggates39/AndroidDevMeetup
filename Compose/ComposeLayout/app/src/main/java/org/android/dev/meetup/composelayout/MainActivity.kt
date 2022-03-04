388abcd98dbebc46aa7dd8f2db44b85c95c05f40
Marshall Gates <mggates@gmail.com>
2022-03-03 19:17:33 -0500

    Starting an intrincsic study

--- Compose/ComposeLayout/app/src/main/java/org/android/dev/meetup/composelayout/MainActivity.kt
+++ Compose/ComposeLayout/app/src/main/java/org/android/dev/meetup/composelayout/MainActivity.kt
@@ -376,3 +376,36 @@ private fun decoupledConstraints(margin: Dp): ConstraintSet {
         }
     }
 }
+
+@Composable
+fun TwoTexts(modifier: Modifier = Modifier, text1: String, text2: String) {
+    Row(modifier = modifier) {
+        Text(
+            modifier = Modifier
+                .weight(1f)
+                .padding(start = 4.dp)
+                .wrapContentWidth(Alignment.Start),
+            text = text1
+        )
+
+        Divider(color = Color.Black, modifier = Modifier.fillMaxHeight().width(1.dp))
+        Text(
+            modifier = Modifier
+                .weight(1f)
+                .padding(end = 4.dp)
+                .wrapContentWidth(Alignment.End),
+
+            text = text2
+        )
+    }
+}
+
+@Preview
+@Composable
+fun TwoTextsPreview() {
+    ComposeLayoutTheme {
+        Surface {
+            TwoTexts(text1 = "Hi", text2 = "there")
+        }
+    }
+}
