77e24afcc2cbfb1673b495b0a1df2a5fe1493c2d
Marshall Gates <mggates@gmail.com>
2022-04-14 20:08:08 -0400

    Now it uses Kotlin's delegated properties.on the count

--- Compose/BasicStateCodelab/app/src/main/java/org/android/dev/meetup/basicstatecodelab/WaterCounter.kt
+++ Compose/BasicStateCodelab/app/src/main/java/org/android/dev/meetup/basicstatecodelab/WaterCounter.kt
@@ -1,24 +1,20 @@
 package org.android.dev.meetup.basicstatecodelab
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.compose.material.Button
-import androidx.compose.foundation.layout.Column
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        // Changes to count are now tracked by Compose
-        val count: MutableState<Int> = remember { mutableStateOf(0) }
+        var count by remember { mutableStateOf(0) }
 
-        Text("You've had ${count.value} glasses.")
-        Button(onClick = { count.value++ }, Modifier.padding(top = 8.dp)) {
+        Text("You've had $count glasses.")
+        Button(onClick = { count++ }, Modifier.padding(top = 8.dp)) {
             Text("Add one")
         }
     }
