976f71a678438bb0e39e9383f422222e2f22ea2f
Marshall Gates <mggates@gmail.com>
2022-04-14 20:04:13 -0400

    First State change now works

---
+++ Compose/BasicStateCodelab/app/src/main/java/org/android/dev/meetup/basicstatecodelab/WellnexxScreen.kt
@@ -0,0 +1,9 @@
+package org.android.dev.meetup.basicstatecodelab
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun WellnessScreen(modifier: Modifier = Modifier) {
+    WaterCounter(modifier)
+}
