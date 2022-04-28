bb40a899a17095b868f253a3e7a130308abfff4a
Marshall Gates <mggates@gmail.com>
2022-04-28 19:47:49 -0400

    Refactor water counter into a stateless composable and a stateful composab.e

--- Compose/BasicStateCodelab/app/src/main/java/org/android/dev/meetup/basicstatecodelab/WaterCounter.kt
+++ Compose/BasicStateCodelab/app/src/main/java/org/android/dev/meetup/basicstatecodelab/WaterCounter.kt
@@ -14,6 +14,23 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
+fun StatefulCounter(modifier: Modifier = Modifier) {
+    var count by rememberSaveable { mutableStateOf(0) }
+    StatelessCounter(count, { count++ }, modifier)
+}
+
+@Composable
+fun StatelessCounter(count: Int, onIncrement: () -> Unit, modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(16.dp)) {
+        if (count > 0) {
+            Text("You've had $count glasses.")
+        }
+        Button(onClick = onIncrement, Modifier.padding(top = 8.dp), enabled = count < 10) {
+            Text("Add one")
+        }
+    }
+}
+@Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
         var count by rememberSaveable { mutableStateOf(0) }
