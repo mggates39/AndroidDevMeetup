b4ba869895b43df4018fe3b6c99307941e51f8f6
Marshall Gates <mggates@gmail.com>
2022-04-28 20:28:16 -0400

    Added list state to remove items Section 11

--- Compose/BasicStateCodelab/app/src/main/java/org/android/dev/meetup/basicstatecodelab/WellnessTasksList.kt
+++ Compose/BasicStateCodelab/app/src/main/java/org/android/dev/meetup/basicstatecodelab/WellnessTasksList.kt
@@ -8,16 +8,16 @@ import androidx.compose.ui.Modifier
 
 @Composable
 fun WellnessTasksList(
-    modifier: Modifier = Modifier,
-    list: List<WellnessTask> = remember { getWellnessTasks() }
+    list: List<WellnessTask>,
+    onCloseTask: (WellnessTask) -> Unit,
+    modifier: Modifier = Modifier
 ) {
-    LazyColumn(
-        modifier = modifier
-    ) {
-        items(list) { task ->
-            WellnessTaskItem(taskName = task.label)
+    LazyColumn(modifier = modifier) {
+        items(
+            items = list,
+            key = { task -> task.id }
+        ) { task ->
+            WellnessTaskItem(taskName = task.label, onClose = { onCloseTask(task) })
         }
     }
 }
-
-private fun getWellnessTasks() = List(30) { i -> WellnessTask(i, "Task # $i") }
